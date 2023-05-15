c5582f8a673cece410d67e89de90963e8d883443
mutatrum <mutatrum@gmail.com>
2023-05-15 16:05:34 +0200

    Handle NASDAQ error

--- src/gold/prices.js
+++ src/gold/prices.js
@@ -41,6 +41,10 @@ module.exports = function() {
   }
 
   function process(body) {
+    if (!body.dataset_data) {
+      logger.log(`Error: ${body}`)
+      return
+    }
     if (!body.dataset_data.hasOwnProperty('column_names')) {
       logger.log(`Error: ${body}`)
       return
